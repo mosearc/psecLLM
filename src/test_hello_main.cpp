8077d38587c96c96a5e7b4cc739233f8f2a74789
Debian <mose.arcaro@gmail.com>
2026-02-19 17:40:59 +0000

    fixmain

--- src/test_hello_main.cpp
+++ src/test_hello_main.cpp
@@ -1,8 +1,8 @@
 #include "Obfusk8/Instrumentation/materialization/state/Obfusk8Core.hpp"
 
-_main({
-
 #if defined(OBF_LEVEL_HEAVY)
+
+_main({
     // heavy: string encryption + bogus flow + MBA arithmetic
     OBF_BOGUS_FLOW_LABYRINTH
     NOP()
@@ -10,19 +10,24 @@ _main({
     auto s2 = OBFUSCATE_STRING("2 + 3 = %d\n");
     printf("%s\n", s1.c_str());
     printf(s2.c_str(), OBF_MBA_ADD(2, 3));
+})
 
 #elif defined(OBF_LEVEL_MEDIUM)
+
+_main({
     // medium: string encryption only
     auto s1 = OBFUSCATE_STRING("Hello from test_hello.cpp");
     auto s2 = OBFUSCATE_STRING("2 + 3 = %d\n");
     printf("%s\n", s1.c_str());
     printf(s2.c_str(), 2 + 3);
+})
 
 #else
+
+_main({
     // light: just _main VM wrapper, no extra macros
     printf("Hello from test_hello.cpp\n");
     printf("2 + 3 = %d\n", 2 + 3);
+})
 
 #endif
-
-})
