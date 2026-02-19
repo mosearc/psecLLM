c7624192f256e3f374036b4df92a21cadebc335c
Debian <mose.arcaro@gmail.com>
2026-02-19 09:25:28 +0000

    test complete

--- src/test_hello.cpp
+++ src/test_hello.cpp
@@ -1,7 +1,7 @@
 // Minimal Obfusk8 test
 // No _main wrapper for fast compile verification
 // To test the full VM engine, change to:
-//   #include "Obfusk8/Instrumentation/materialization/state/Obfusk8Core.hpp"
+#include "Obfusk8/Instrumentation/materialization/state/Obfusk8Core.hpp"
 //   _main({ printf("Hello from test_hello.cpp\n"); printf("2 + 3 = %d\n", 2 + 3); })
 #include <cstdio>
 
